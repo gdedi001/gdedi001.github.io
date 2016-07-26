79b27799893e9f122e34ce6d5d625add65574baf
De diego, Gabriel X <gabriel.dediego@contractor.mybrighthouse.com>
2016-07-26 10:53:16 -0400

    copy og files

---
+++ js/app.js
@@ -0,0 +1,29 @@
+'use strict';
+var brightHouse = document.querySelector("#brightHouse"),
+    cloud = document.querySelector("#cloud"),
+    internet = document.querySelector("#internet"),
+    security = document.querySelector("#security"),
+    firewall = document.querySelector("#firewall");
+
+
+//brightHouse.onclick = function(){
+//    this.setAttribute('position', '0.00 5.00 -5');
+//};
+
+brightHouse.addEventListener('click', function(){
+    //this.setAtt
+    //this.setAttribute();
+    //this.setAttribute('position', '0.00 5.00 -5');
+});
+
+internet.onclick = function(){
+    this.setAttribute('material', 'color', 'white');
+};
+
+security.onclick = function(){
+    
+};
+
+cloud.onclick = function(){
+    
+};
