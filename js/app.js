1c0ff5364ac54b3cc94393344c259a26c35ee78e
De diego, Gabriel X <gabriel.dediego@contractor.mybrighthouse.com>
2016-07-31 01:11:24 -0400

    cp test

--- js/app.js
+++ js/app.js
@@ -3,21 +3,16 @@ var brightHouse = document.querySelector("#brightHouse"),
     cloud = document.querySelector("#cloudHolder"),
     internet = document.querySelector("#internetHolder"),
     security = document.querySelector("#securityHolder"),
-    firewall = document.querySelector("#firewallHolder");
+    firewall = document.querySelector("#firewallHolder"),
+    cloudExit = document.querySelector("#exit");
 
-//document.querySelector('#cloudShadow').addEventListener('click', function(){
-//    this.setAttribute('position', '0.00 5.00 -5.00');
-//    console.log('test complete');
-//});
+
+function cloudActivate(){
+    document.querySelector('#cloud-yoyo').emit('yoyo');
+    document.querySelector('#cloudTextFade').emit('fade');
+}
 
 
 // Trigger Cloud Server model to appear
-cloud.addEventListener('click', function(){
-    document.querySelector('#yoyo').emit('move');
-    console.log('testing!');
-});
-
-// Trigger an event to begin fading.
-//documet.querySelector('#test').addEventListener('click', function(){
-//    document.querySelector('#fading-cube').emit('fade');
-//});
+cloud.addEventListener('click', cloudActivate);
+cloudExit.addEventListener('click', cloudActivate);
