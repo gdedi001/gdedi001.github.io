9302a2c8209605808f1cfab5f782258fa54f4423
De diego, Gabriel X <gabriel.dediego@contractor.mybrighthouse.com>
2016-08-02 15:36:03 -0400

    update to prototype

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ var brightHouse = document.querySelector("#brightHouse"),
     internet = document.querySelector("#internetHolder"),
     security = document.querySelector("#securityHolder"),
     firewall = document.querySelector("#firewallHolder"),
-    cloudExit = document.querySelector("#exit");
+    cloudExit = document.querySelector("#exit"),
+    powerStart = document.querySelector("#bhnPower");
+
 
 
 function cloudActivate(){
@@ -16,3 +18,12 @@ function cloudActivate(){
 // Trigger Cloud Server model to appear
 cloud.addEventListener('click', cloudActivate);
 cloudExit.addEventListener('click', cloudActivate);
+
+powerStart.addEventListener('click', function(){
+    document.querySelector('#elevator-scan').emit('scan');
+    setTimeout(function(){
+       document.querySelector('#elevator-descent').emit('descent');
+    }, 5000);
+});
+
+//sound="src: sounds/welcome.mp3; on: click; autoplay: false"
